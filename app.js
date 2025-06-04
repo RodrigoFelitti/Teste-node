cb60010290adf0d31c1f24950102f1573d965ae4
Rodrigo Felitti <rodrigofelitti@gmail.com>
2025-06-04 17:32:56 -0300

    Creating base task model

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 import express from 'express';
-import ejs from 'ejs';
-import path, {dirname} from 'path'
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import Task from './backend/models/taskModel.js';
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'frontend', 'views'));
 
 app.use(express.static('public'));
+
+const tasks = [];
+
+const task1 = new Task(1, 'Estudar para a prova', 'Capítulo 5 de Algoritmos');
+const task2 = new Task(2, 'Enviar relatório', 'Até sexta-feira');
+
+tasks.push(task1, task2);
 
 app.get('/', (request, response) => {
     response.redirect('/testIndex');
 });
 
 app.get('/testIndex', (request, response) => {
-    response.render('testIndex');
+    response.render("testIndex", { tasks });
 });
 
 app.listen(8080, () => {
-    console.log("Iniciei o servidor da silva")
+    console.log("Iniciei o servidor da silva");
 });
